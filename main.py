8440d4978cf16eba5b51a2cad0c4e2321d653d9f
HYEOK_E <jhjeong00@gmail.com>
2021-05-18 15:19:07 +0900

    fourier graph clear

--- main.py
+++ main.py
@@ -1,13 +1,64 @@
-from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+plt.subplot(2,3,1)
 
-# t = np.linspace(-10, 10, 500)
-# plt.plot(t, signal.square(2 * np.pi * 5 * t),'b')
-# plt.ylim(-2, 2)
-plt.grid(0.5)
-# plt.show()
-plt.plot([-10,-5, 0, 5, 10], [0,0,1,0,0])
-plt.xlim(-10, 10)
-plt.ylim(0.5, 1.5)
+x = np.linspace(-10, 10, 5000)
+
+y = (np.where((x<-0.5 )| (x>0.5), 0, 1))
+plt.axis([-10, 10, -0.5, 1.5])
+plt.plot(x, y)
+plt.grid()
+
+plt.subplot(2,3,2)
+x = np.linspace(-10, 10, 5000)
+y = (np.where((x<-2.5 )| (x>2.5), 0, 1))
+plt.axis([-10, 10, -0.5, 1.5])
+plt.plot(x, y)
+plt.grid()
+
+plt.subplot(2,3,3)
+x = np.linspace(-10, 10, 5000)
+
+y = (np.where((x<-5 )| (x>5), 0, 1))
+plt.axis([-10, 10, -0.5, 1.5])
+plt.plot(x, y)
+plt.grid()
+
+plt.subplot(2,3,4)
+x = np.linspace(-3, 3, 5000)
+y = np.sin(np.pi*x)/(x*np.pi)
+plt.plot(x, y)
+plt.grid()
+
+plt.subplot(2,3,5)
+x = np.linspace(-3, 3, 5000)
+y = (np.sin(np.pi*x*5))/(x*np.pi*5)
+plt.plot(x, y)
+plt.grid()
+
+plt.subplot(2,3,6)
+x = np.linspace(-3, 3, 5000)
+y = (np.sin(np.pi*x*10))/(x*np.pi*10)
+plt.plot(x, y)
+
+plt.grid()
 plt.show()
+
+plt.subplot(1,2,1)
+x = np.linspace(-0.1, 0.1, 5000)
+
+y = (np.where((x>0.05 )| (x<-0.05), 0, 1)) * (np.cos(2 * np.pi * x * 100))
+plt.axis([-0.1, 0.1, -1.5, 1.5])
+plt.plot(x, y)
+plt.grid()
+
+plt.subplot(1,2,2)
+x = np.linspace(-200, 200, 5000)
+y1 = (0.5)*((np.sin((0.1*(x-100))*np.pi))/(np.pi*(0.1*(x-100)))
+            +(np.sin((0.1*(x+100))*np.pi))/(np.pi*(0.1*(x+100))))
+y = np.where(y1 < 0, -y1, y1)
+plt.axis([-200, 200, 0, 0.6])
+plt.plot(x, y)
+plt.grid()
+plt.show()
+
